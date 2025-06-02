a10d099e8d9f4b1f9701da500f10e807af37c111
sungwoo <woalals009@gmail.com>
2025-06-02 16:45:53 +0900

    dao 추가 serviceInfo 추가

---
+++ JDBCProject/src/com/gh/dao/CustomerDAO.java
@@ -0,0 +1,51 @@
+package com.gh.dao;
+
+import java.sql.Date;
+import java.util.List;
+
+import com.gh.vo.Customer;
+import com.gh.vo.GuestHouse;
+import com.gh.vo.Reservation;
+
+public interface CustomerDAO {
+	// 1. 회원가입
+	public boolean registerCustomer(Customer customer);
+
+	// 2. 회원 정보 수정
+	public boolean updateCustomerInfo(Customer customer);
+
+	// 3. 회원 삭제
+	public boolean deleteCustomer(int customerId);
+
+	// 4. 예약하기
+	public boolean addReservation(Reservation reservation);
+
+	// 5. 예약 수정
+	public boolean updateReservation(Reservation reservation);
+
+	// 6. 예약 취소
+	public boolean cancelReservation(int reservationId);
+
+	// 7. 예약 조회 (본인 예약 내역)
+	public List<Reservation> getMyReservations(int customerId);
+
+	// 8. 게스트하우스 지역별 조회
+	public List<GuestHouse> getGuestHousesByRegion(String region);
+
+	// 9. 회원 등급 확인 및 할인 적용
+	public String getCustomerGrade(int customerId);
+
+	// 10. 게스트하우스 전체 보기
+	public List<GuestHouse> getAllGuestHouses();
+
+	// 11. 게스트하우스 남은 인원 확인
+	public int getRemainingCapacity(String guestHouseName);
+
+	// 12. 게스트하우스 특성 조건 필터
+	public List<GuestHouse> filterGuestHouses(String keyword);
+
+	// 13. 요일에 따른 요금 계산
+	public int calculatePriceByDay(String guestHouseName, Date date);
+
+
+}
---
+++ JDBCProject/src/config/ServerInfo.java
@@ -0,0 +1,9 @@
+package config;
+
+public interface ServerInfo {
+	// public static final
+	String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+	String URL = "jdbc:mysql://127.0.0.1:3306/kosa?serverTimezone=UTC&useUnicode=yes&characterEncoding=UTF-8";
+	String USER = "root";
+	String PASS = "1234";
+}
